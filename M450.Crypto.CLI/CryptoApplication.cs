9eddfc3c0bef99e28acfe222d877e94f3e549d16
Robin Kuettel <robin.kuettel@roche.com>
2024-01-11 14:07:56 +0100

    Improve code

--- M450.Crypto.CLI/CryptoApplication.cs
+++ M450.Crypto.CLI/CryptoApplication.cs
@@ -1,7 +1,5 @@
-using CommandLine;
 using M450.Crypto.DLL;
 using M450.Crypto.DLL.Cryptos;
-using OpenQA.Selenium;
 
 namespace M450.Crypto.CLI;
 public class CryptoApplication
@@ -23,12 +21,22 @@ public class CryptoApplication
         }
         else
         {
+            //only initialize webdriver when needed
+            //hide console output during initialisation
             var currentConsole = Console.Out;
             Console.SetOut(TextWriter.Null);
             DLL.WebDriverManager.Initialize();
             Console.SetOut(currentConsole);
-            
-            RunGetPriceCommand(arguments);
+
+            if (arguments.Volume)
+            {
+                RunGetTransactionVolumeCommand(arguments);
+            }
+
+            if (arguments.Price)
+            {
+                RunGetPriceCommand(arguments);
+            }
             
             DLL.WebDriverManager.Destroy();
         }
@@ -39,20 +47,20 @@ public class CryptoApplication
         var service = cryptos.Find(x => x.Currency == arguments.CryptoCurrency);
         if (service == null)
         {
-            console.WriteError($"Error: Currency: \"{arguments.CryptoCurrency}\" is not supported.");
+            Console.WriteLine($"Error: Currency: \"{arguments.CryptoCurrency}\" is not implemented.");
             return;
         }
-        console.WriteLine($"Price of {arguments.CryptoCurrency} is currently {service.GetCurrentPrice()}$");
+        console.WriteLine($"Price of {arguments.CryptoCurrency} is currently {service.GetCurrentPrice():N1}$");
     }
     private void RunGetTransactionVolumeCommand(CryptoArguments arguments)
     {
         var service = cryptos.Find(x => x.Currency == arguments.CryptoCurrency);
         if (service == null)
         {
-            Console.WriteLine($"Error: Currency: \"{arguments.CryptoCurrency}\" is not supported.");
+            Console.WriteLine($"Error: Currency: \"{arguments.CryptoCurrency}\" is not implemented.");
             return;
         }
-        Console.WriteLine($"Price of {arguments.CryptoCurrency} is currently {service.GetTransactionVolume()}$");
+        Console.WriteLine($"24h Transaction Volume of {arguments.CryptoCurrency} is currently {service.GetTransactionVolume():N1}$");
     }
     
     private void RunListCommand()
--- M450.Crypto.DLL/Cryptos/BitcoinData.cs
+++ M450.Crypto.DLL/Cryptos/BitcoinData.cs
@@ -13,8 +13,18 @@ public class BitcoinData : ICryptoData
         driver.Navigate().GoToUrl("https://www.blockchain.com/explorer/assets/btc");
         var element = driver.FindElement(By.ClassName("sc-bb87d037-10"));
         var value = element.Text.Replace("$", "").Replace(",", "");
-        decimal price = decimal.Parse(value);
-        return price;
+
+        try
+        {
+            decimal price = decimal.Parse(value);
+            return price;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Internal Error: {e.ToString()}");
+        }
+
+        return -1;
     }
 
     public decimal GetTransactionVolume()
@@ -22,10 +32,18 @@ public class BitcoinData : ICryptoData
         driver.Navigate().GoToUrl("https://www.blockchain.com/explorer/assets/btc");
         var element =
             driver.FindElement(By.XPath("//*[@id=\"__next\"]/div[2]/div[2]/main/div/div/div[4]/div[2]/div[3]/div[2]"));
-        var value = element.Text;
-        decimal price = decimal.Parse(value);
+        var value = element.Text.Replace("$", "").Replace(",", "");
+        try
+        {
+            decimal volume = decimal.Parse(value);
+            return volume;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Internal Error: {e.ToString()}");
+        }
 
-        return price;
+        return -1;
     }
 
     public decimal GetTransactionFees(TimeSpan s)
